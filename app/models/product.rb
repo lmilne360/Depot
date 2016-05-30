0b789ba06e37e06cf5ed801872513aac76e0bcc0
Lmilne <lmilne360@gmail.com>
2016-05-30 01:46:03 -0400

    Adding a button

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 	# Makes sure these fields are not empty
 	validates :title, :description, :image_url, presence: true
@@ -15,8 +18,21 @@ class Product < ActiveRecord::Base
 		message: 'must be a URL for GIF, JPG or PNG image'
 	}
 
+
 	def self.latest
 		Product.order(:updated_at).last	
+	end
+
+	private
+
+	#ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end	
 	end
 
 end
